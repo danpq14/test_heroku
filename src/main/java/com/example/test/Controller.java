d625795f63a310f81fc8fbd322d5946f916306fc
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-07-09 11:10:13 +0700

    ...

--- src/main/java/com/example/test/Controller.java
+++ src/main/java/com/example/test/Controller.java
@@ -1,12 +1,18 @@
 package com.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/abc")
+    @GetMapping("/a")
     public String index(){
         return "index";
     }
+    @GetMapping("/b")
+    public String index1(){
+        return "goodbye";
+    }
+
 }
